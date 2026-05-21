ba21c7432be452a7af762db37c0827b611092712
muhamadadrian055-design <muhamadadrian055@gmail.com>
2026-05-21 08:08:11 +0700

    Update data.js

--- api/data.js
+++ api/data.js
@@ -1,10 +1,8 @@
 export default function handler(req, res) {
-  return res.status(200).json(
-    global.latestDonation || {
-      supporter: null,
-      amount: 0,
-      message: "",
-      created_at: null
-    }
-  );
+  return res.status(200).json(global.latestDonation || {
+    supporter: null,
+    amount: 0,
+    message: "",
+    created_at: null
+  });
 }
